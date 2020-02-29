122951344eca3261e9352f7534dfc6c11a503ea0
David Lassner <davidlassner@gmail.com>
2020-02-29 14:59:22 +0100

    add utils installer to notebooks for colab compatibility

--- material/notebooks/bias_ml_dh_utils/setup.py
+++ material/notebooks/bias_ml_dh_utils/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bias_ml_dh_utils',
       version='0.1',
       description='helper functions for the workshop on bias in ml and dh',
-      url='https://github.com/millawell/bias-ml-dh/tree/master/material/notebooks/utils',
+      url='https://github.com/millawell/bias-ml-dh/',
       author='Stephanie Brandl and David Lassner',
       author_email='lassner@tu-berlin.de',
       license='MIT',
