5ea93e6f6009c9f64f011bfa82bfe8b14cb589ee
David Lassner <davidlassner@gmail.com>
2020-02-29 14:51:04 +0100

    rename module in setup.py

--- material/notebooks/bias_ml_dh_utils/setup.py
+++ material/notebooks/bias_ml_dh_utils/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='bias-ml-dh-utils',
+setup(name='bias_ml-dh-utils',
       version='0.1',
       description='helper functions for the workshop on bias in ml and dh',
       url='https://github.com/millawell/bias-ml-dh/tree/master/material/notebooks/utils',
       author='Stephanie Brandl and David Lassner',
       author_email='lassner@tu-berlin.de',
       license='MIT',
-      packages=['bias-ml-dh-utils'],
+      packages=['bias_ml_dh_utils'],
       zip_safe=False)
